b1ed1a06195fbf19603028cc0be4389427927851
paulshim100 <74570815+paulshim100@users.noreply.github.com>
2021-02-15 22:59:54 -0800

    Update workplace.py

--- workplace.py
+++ workplace.py
@@ -1,19 +1,3 @@
 def treasureNecklace(necklace):
   # write your code here
-  hold = necklace[0]
-  largest = 0
-  group = 0
-
-  for bead in necklace:
-    if bead == hold:
-      group += 1
-    else:
-      if group > largest:
-        largest = group
-      group = 1
-      hold = bead
-
-  if group > largest:
-    largest = group
-
-  return str(largest) + "\n"
+  
